8e3d90bc0f1dcf5697cd048d03ef14fab9a63e88
immortalvoyage <immortalvoyage@gmail.com>
2026-08-11 02:53:10 +0800

    feat: 新增玩家角色啟動流程
    * feat: 新增玩家角色啟動流程
    
    * feat: 匯出角色啟動流程
    
    * test: 增加玩家角色啟動流程測試

--- src/modules/character/index.js
+++ src/modules/character/index.js
@@ -4,3 +4,4 @@ export { buildCheckModifier, buildSurvivalModifiers } from './modifiers.js';
 export { ORIGIN_PREFERENCES, selectBirthRegion, createCharacterFromBirth } from './character-creation.js';
 export { createMemoryCharacterRepository } from './character-repository.js';
 export { createD1CharacterRepository } from './d1-character-repository.js';
+export { createPlayerCharacterBootstrap } from './player-character-bootstrap.js';
