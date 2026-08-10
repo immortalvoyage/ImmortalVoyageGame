659ca5355d5c1d1717aaa5ef6930e517958186f3
immortalvoyage <immortalvoyage@gmail.com>
2026-08-10 23:36:13 +0800

    feat: 建立核心入口

---
+++ src/core/index.js
@@ -0,0 +1,6 @@
+export { WorldClock } from "./world-clock.js";
+export { EventBus } from "./event-bus.js";
+export { FeatureFlags } from "./feature-flags.js";
+export { ModuleRegistry } from "./module-registry.js";
+export { ActionResolver } from "./action-resolver.js";
+export { CORE_VERSION, WORLD_SCHEMA_VERSION, SAVE_SCHEMA_VERSION, getSchemaInfo } from "./schema.js";
