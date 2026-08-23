97754c77beecbe3c6f82ff48b810a6de77275923
immortalvoyage <immortalvoyage@gmail.com>
2026-08-24 00:48:48 +0800

    feat: wire purpose module into V2 runtime

--- src/game.js
+++ src/game.js
@@ -6,11 +6,12 @@ import { characterModule } from './modules/character/index.js';
 import { inventoryModule } from './modules/inventory/index.js';
 import { locationModule } from './modules/location/index.js';
 import { npcModule } from './modules/npc/index.js';
+import { purposeModule } from './modules/purpose/index.js';
 import { survivalModule } from './modules/survival/index.js';
 import { economyModule } from './modules/economy/index.js';
 import { narrativeModule } from './modules/narrative/index.js';
 
-const allModules = [characterModule, inventoryModule, locationModule, npcModule, survivalModule, economyModule, narrativeModule];
+const allModules = [characterModule, inventoryModule, locationModule, npcModule, purposeModule, survivalModule, economyModule, narrativeModule];
 
 export function createGame({ store, now = () => Date.now(), enabledModules = allModules.map((module) => module.manifest.name) }) {
   if (!store) throw new TypeError('store is required');
